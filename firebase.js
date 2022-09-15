f70c4a6c1c2fb1bf1aadcf83394fdd33f112dd91
Amadou Oury Sow <saos@abatgroup.de>
2022-09-15 23:09:59 +0200

    Abgabe.

--- firebase.js
+++ firebase.js
@@ -2,13 +2,18 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth";
 import {initializeFirestore} from 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: "AIzaSyDCENsh0tZlNtbcNAZZHqt1RtkNIsWsNuE",
-  authDomain: "monmarhe.firebaseapp.com",
-  projectId: "monmarhe",
-  storageBucket: "monmarhe.appspot.com",
-  messagingSenderId: "810364309186",
-  appId: "1:810364309186:web:29c2ffbcdf7bb17cdb755b",
-  measurementId: "G-PZ6PL043EE"
+
+  apiKey: apiKey,
+  
+  authDomain: authDomain,
+
+  projectId: projectId,
+
+  storageBucket: storageBucket,
+
+  messagingSenderId: messagingSenderId,
+
+  appId: appId
 };
 
 // Initialize Firebase
